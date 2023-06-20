1d3540588d8663e9e1b42560d8e3e47933625b8d
MarynaShchedrova <shchedrovamaryna@gmail.com>
2023-06-20 14:16:58 +0300

    update

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,4 +1,5 @@
 import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 import { logIn } from 'redux/auth/operations';
 import { StyledForm, StyledLabel } from './LoginForm.styled';
 import { Button, Input } from '@chakra-ui/react';
@@ -6,29 +7,52 @@ import { Button, Input } from '@chakra-ui/react';
 export const LoginForm = () => {
   const dispatch = useDispatch();
 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const handleSubmit = e => {
     e.preventDefault();
-    const form = e.currentTarget;
-    dispatch(
-      logIn({
-        email: form.elements.email.value,
-        password: form.elements.password.value,
-      })
-    );
-    form.reset();
+    dispatch(logIn({ email, password }));
+    setEmail('');
+    setPassword('');
   };
+  // const handleSubmit = e => {
+  //   e.preventDefault();
+  //   const form = e.currentTarget;
+  //   dispatch(
+  //     logIn({
+  //       email: form.elements.email.value,
+  //       password: form.elements.password.value,
+  //     })
+  //   );
+  //   form.reset();
+  // };
 
   return (
     <StyledForm onSubmit={handleSubmit} autoComplete="off">
       <StyledLabel>
         Email
-        <Input type="email" name="email" />
+        <Input
+          type="email"
+          name="email"
+          required
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
       </StyledLabel>
       <StyledLabel>
         Password
-        <Input type="password" name="password" />
+        <Input
+          type="password"
+          name="password"
+          required
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
       </StyledLabel>
-      <Button colorScheme='blue' type="submit">Log In</Button>
+      <Button colorScheme="blue" type="submit">
+        Log In
+      </Button>
     </StyledForm>
   );
 };
